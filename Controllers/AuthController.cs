bf2989104f3086ed2cd98b21f14b65176367fd7a
venkateshpmsquare <venkatesh.siet@gmail.com>
2025-03-01 22:42:35 +0530

    claro-auth-service → Authentication & Authorization - Completed

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Claro.AuthService.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Claro.AuthService.Application.Contracts.Authentication;
+using Claro.AuthService.Domain.Entities;
+using Claro.AuthService.Application.Helpers;
+namespace Claro.AuthService.Api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly PasswordHasher _passwordHasher;
+        public AuthController(IAuthService authService, PasswordHasher passwordHasher)
+        {
+            _authService = authService;
+            _passwordHasher = passwordHasher;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Application.Contracts.Authentication.RegisterRequest request)
+        {
+            // Check if the user already exists
+            var existingUser = await _authService.GetByUsernameAsync(request.Username);
+            if (existingUser != null)
+            {
+                return BadRequest("User already exists.");
+            }
+
+            // Hash the password
+            var hashedPassword = _passwordHasher.HashPassword(request.Password);
+
+            // Create new user entity
+            var user = new User
+            {
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = hashedPassword,
+                Role = request.Role
+            };
+            // Store user in database
+            await _authService.AddAsync(user);
+
+            return Ok("User registered successfully.");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Application.Contracts.Authentication.LoginRequest request)
+        {
+            var token = await _authService.AuthenticateAsync(request.Username, request.Password);
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(new { Token = token });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,41 +1,90 @@
+using Claro.AuthService.Application.Helpers;
+using Claro.AuthService.Application.Interfaces;
+using Claro.AuthService.Application.Services;
+using Claro.AuthService.Infrastructure.Persistence;
+using Claro.AuthService.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//add swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+// Add Identity services 
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//                .AddEntityFrameworkStores<ClaroAuthDbContext>()
+//                .AddDefaultTokenProviders();
+
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<ClaroAuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+builder.Services.AddSingleton<PasswordHasher>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<Claro.AuthService.Infrastructure.Interfaces.IUserRepository, UserRepository>();  
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
